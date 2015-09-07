8daaa14b0c4556f745945a76bfc50f1013f95db6
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-07 15:31:04 +0100

    Initial Cucumber Setup\n Added codebreaker_steps.rb to define Cucumber steps\n Added /lib source files that can be referenced via Cucumber

---
+++ features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,33 @@
+#
+# Test Doubles
+#
+class Output2 #Output 2 as RSpec has a built-in Output class
+              #that confuses Cucumber method matching
+  def messages
+    @messages ||= []
+  end
+  def puts(message)
+    messages << message
+  end
+end
+
+def output2
+  @output2 ||= Output2.new
+end
+
+
+#
+# Step Definitions
+#
+
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(output2)
+  game.start
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  output2.messages.should include(message)
+end
