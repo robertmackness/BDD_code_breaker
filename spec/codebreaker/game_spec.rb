90e929ee383f7fd098443fb61b689a6ece66306c
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-08 14:01:58 +0100

    Refactor Cycle - game_spec 1. DRY principles - used RSpec let(object) {block} syntax    instead of defining local method variables twice. 2. Added an executable script to launch game in shell.    chmod +x bin/codebreaker

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -4,15 +4,13 @@ module Codebreaker
 
   describe Game do 
     describe '#start' do
+      let(:output2) {double('output2').as_null_object}
+      let(:game) {Game.new(output2)}
       it "sends a welcome message" do
-        output2 = double('output2')
-        game = Game.new(output2)
         expect(output2).to receive(:puts).with('Welcome to Codebreaker!')
         game.start
       end
       it "prompts for the first guess" do
-        output2 = double("output2")
-        game = Game.new(output2)
         expect(output2).to receive(:puts).with('Enter guess:')
         game.start
       end
